b1bf9258f1dd752ae8d942d3c20c1e3e02e24ab7
mihyeon1234 <82564312+mihyeon1234@users.noreply.github.com>
2022-07-15 16:17:02 +0900

    공백 수정

--- python/수업자료(코드)/lotto.py
+++ python/수업자료(코드)/lotto.py
@@ -17,7 +17,6 @@ for i in range(6):
     if lucky[i] in winner:
         member = member + 1
 
-
 # 무한 루프 for 문으로 로또 맞추기
 l=[1]
 for x in l:
@@ -33,3 +32,5 @@ for x in l:
     else:
         print("Same",x)
         break
+
+    
