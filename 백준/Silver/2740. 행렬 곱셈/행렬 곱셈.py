731326158164452e7ecab95804bc2bc9e25859b2
mihyeon1234 <82564312+mihyeon1234@users.noreply.github.com>
2022-08-13 17:15:20 +0900

    [Silver V] Title: 행렬 곱셈, Time: 240 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.py
@@ -0,0 +1,13 @@
+an, am = map(int, input().split())
+a = [list(map(int, input().split()))for _ in range(an)]
+
+bm, bk = map(int, input().split())
+b = [list(map(int, input().split()))for _ in range(bm)]
+
+arr = [[0]*bk for _ in range(an)]
+for i in range(an):
+    for j in range(bk):
+        for k in range(bm):
+            arr[i][j] += a[i][k]*b[k][j]
+for aa in range(an):
+    print(*arr[aa])
