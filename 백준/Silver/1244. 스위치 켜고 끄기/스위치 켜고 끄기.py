4b9f9eeba0ead69fa95c55dbafbfd92745b572e5
mihyeon1234 <82564312+mihyeon1234@users.noreply.github.com>
2022-08-14 01:30:14 +0900

    [Silver III] Title: 스위치 켜고 끄기, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
@@ -0,0 +1,35 @@
+t = int(input())
+li = list(map(int, input().split()))
+
+stu = int(input())
+for i in range(stu):
+    gen, num = map(int, input().split())
+    if gen == 1:
+        n = 1
+        while n <= t//num:
+            if li[num * n -1] == 0:
+                li[num * n -1] = 1
+                n += 1
+            elif li[num * n -1] == 1:
+                li[num * n -1] = 0
+                n += 1
+    elif gen == 2:
+        if li[(num - 1)] == 0:
+            li[(num - 1)] = 1
+        elif li[(num-1)] == 1:
+            li[(num - 1)] = 0
+        n = 1
+        while True:
+            if num+n > t or num - n < 1:
+                break
+            elif li[num - n -1] == li[num+n-1] == 0:
+                li[num - n-1] = li[num+n-1] = 1
+                n += 1
+            elif li[num-n-1] == li[num+n-1] == 1:
+                li[num - n-1] = li[num+n-1] = 0
+                n += 1
+            else:
+                break
+
+for p in range(0, len(li), 20):
+    print(*li[p:p+20])
