7cd91a38679069c8de4ce583dd9a3d63841e9f82
mihyeon1234 <82564312+mihyeon1234@users.noreply.github.com>
2022-08-16 17:31:11 +0900

    [Bronze II] Title: 음계, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.py
+++ 백준/Bronze/2920. 음계/음계.py
@@ -1,8 +1,7 @@
-num = list(input().split())
-renum = ['8', '7', '6', '5', '4', '3', '2', '1']
-if num == renum :
+num = input()
+if num == '8 7 6 5 4 3 2 1' :
     print('descending')
-elif num == renum[::-1]:
+elif num == '1 2 3 4 5 6 7 8':
     print('ascending')
 else:
     print('mixed')
