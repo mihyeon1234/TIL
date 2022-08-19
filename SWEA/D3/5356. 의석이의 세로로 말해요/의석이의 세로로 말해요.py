e7175ab81545b113af19b62c2ff207dc885040b8
mihyeon1234 <82564312+mihyeon1234@users.noreply.github.com>
2022-08-19 15:15:35 +0900

    [D3] Title: 의석이의 세로로 말해요, Time: 174 ms, Memory: 61,420 KB -BaekjoonHub

---
+++ SWEA/D3/5356. 의석이의 세로로 말해요/의석이의 세로로 말해요.py
@@ -0,0 +1,16 @@
+t = int(input())
+for tt in range(1, t+1):
+    arr = [list(input()) for _ in range(5)]
+    li=[]
+    ans = []
+    for i in range(len(arr)):
+        li.append(len(arr[i]))
+
+    maxli = max(li)
+
+    for k in range(maxli):
+        for l in range(5):
+            if len(arr[l]) > k:
+                ans.append(arr[l][k])
+    re =''.join(ans)
+    print(f'#{tt} {re}')
