71e4ed9de5c7814f53b800b501c1f8701379011e
mihyeon1234 <82564312+mihyeon1234@users.noreply.github.com>
2022-08-21 15:24:02 +0900

    [Bronze I] Title: 색종이, Time: 1752 ms, Memory: 37624 KB -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -6,8 +6,8 @@ n = int(sys.stdin.readline())
 for N in range(1, n+1):
     x, y, x1, y1 = map(int, sys.stdin.readline().split())
     for i in range(x, x+x1):
-        for j in range(y, y+y1):
-            arr[i][j] = N
+        arr[i][y:y+y1] = [N]*y1
+
 
 for N in range(1, n+1):
     su = 0
