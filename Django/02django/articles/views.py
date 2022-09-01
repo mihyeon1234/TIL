92119d483f73e08d1d8e31bd9f9264e94b69b012
이미현 <algus7531@naver.com>
2022-09-01 17:42:42 +0900

    0901

--- Django/02django/articles/views.py
+++ Django/02django/articles/views.py
@@ -13,9 +13,9 @@ def new(request):
   return render(request, 'articles/new.html')
 
 def create(request):
-  title = request.GET.get('title')
-  content = request.GET.get('content')
-
+  title = request.POST.get('title')
+  content = request.POST.get('content')
+  # print("서버도착", title, content)
   #DB에 저장
   #1
   # article = Article()
@@ -30,4 +30,35 @@ def create(request):
   #3
   # Article.objects.create(title = title, content = content)
   # return render(request, 'articles/create.html')
+  # create 페이지가 굳이 필요 없음으로 없애고 테이블 값을 입력받은 후
+  # index 페이지로 바로 이동
+  return redirect('articles:detail', article.pk)
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article' : article,
+      }
+    return render(request, 'articles/detail.html', context)
+
+    # return redirect('articles:detail', article.pk)
+
+def delete(request, pk):
+  article = Article.objects.get(pk=pk)
+  article.delete()
   return redirect('articles:index')
+
+def edit(request, pk):
+  article = Article.objects.get(pk=pk)
+  context = {
+    'article' : article,
+  }
+  return render(request, 'articles/edit.html', context)
+
+def update(request, pk):
+  article = Article.objects.get(pk=pk)
+  article.title = request.POST.get('title')
+  article.content = request.POST.get('content')
+  article.save()
+  return redirect('articles:detail', article.pk)
+
