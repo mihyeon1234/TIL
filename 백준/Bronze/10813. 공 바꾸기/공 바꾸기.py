3491b006127d7b817fd40bdb629fcb59e643dfc1
mihyeon1234 <82564312+mihyeon1234@users.noreply.github.com>
2023-02-23 22:30:14 +0900

    [Bronze II] Title: 공 바꾸기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -1,11 +1,13 @@
-import sys
-
-m, n = map(int, sys.stdin.readline().split())
+from sys import stdin
+input = stdin.readline
+m, n = map(int, input().split())
 
 li = list(range(m+1))
 
 for i in range(n):
-    a, b = map(int, sys.stdin.readline().split())
-    li[a] , li[b] = li[b], li[a]
-
-print(*li[1:])
+    a, b = map(int, input().split())
+    # li[a], li[b] = li[b], li[a]
+    tmp = li[a]
+    li[a] = li[b]
+    li[b] = tmp
+print(*li[1:], sep=' ')
