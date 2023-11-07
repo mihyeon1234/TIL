b70f8ae2eed764cf5975538aff330d4e30eda20f
mihyeon1234 <82564312+mihyeon1234@users.noreply.github.com>
2023-11-07 21:53:14 +0900

    [level unrated] Title: 문자열 반복해서 출력하기, Time: 10.97 ms, Memory: 7.39 MB -BaekjoonHub

--- 프로그래머스/unrated/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.py
+++ 프로그래머스/unrated/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.py
@@ -1,11 +1,13 @@
 a, b = input().strip().split(' ')
 b = int(b)
+for i in range(b):
+    print(a, end="")
 # ans=""
 # for i in range(b):
 #     ans += a
 # print(ans)
 
-def fun(a, b):
-    ans = a*b
-    return ans
-print(fun(a, b))
+# def fun(a, b):
+#     ans = a*b
+#     return ans
+# print(fun(a, b))
