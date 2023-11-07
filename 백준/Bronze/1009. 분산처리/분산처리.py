3d715cbf10df870ad31eab27c1f41b3ebf4f2873
mihyeon1234 <82564312+mihyeon1234@users.noreply.github.com>
2023-11-08 02:27:58 +0900

    [Bronze II] Title: 분산처리, Time: 76 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1009. 분산처리/분산처리.py
+++ 백준/Bronze/1009. 분산처리/분산처리.py
@@ -1,25 +1,28 @@
-t = int(input())
-a2 = [6,2,4,8]
-a3 = [1,3,9,7]
-a4 = [6,4]
-a7 = [1,7,9,3]
-a8 = [6,8,4,2]
-a9 = [1,9]
-for i in range(t):
-    n,m = map(int,input().split())
-    if n%10 == 0 :
-        print(10)
-    elif n%10 == 2:
-        print(a2[m%4])
-    elif n%10 == 3:
-        print(a3[m%4])  
-    elif n%10 == 4:
-        print(a4[m%2])   
-    elif n%10 == 1 or n%10 == 5 or n%10 == 6:
-        print(n%10)
-    elif n%10 == 7:
-        print(a7[m%4]) 
-    elif n%10 == 8:
-        print(a8[m%4])  
-    elif n%10 == 9:
-        print(a9[m%2]) 
+# 거듭제곱을 계산하는 함수를 정의합니다.
+def fast_power(base, power, mod):
+    result = 1
+    while power > 0:
+        if power % 2 == 1:
+            result = (result * base) % mod
+        base = (base * base) % mod
+        power = power // 2
+    return result
+
+# 테스트 케이스의 개수 T를 입력받습니다.
+T = int(input())
+
+# 각 테스트 케이스에 대해 반복합니다.
+for _ in range(T):
+    a, b = map(int, input().split())
+
+    # 데이터의 총 개수를 계산합니다.
+    total_data = fast_power(a, b, 10)
+
+    # 데이터의 총 개수를 10으로 나눈 나머지가 마지막 데이터가 처리될 컴퓨터 번호입니다.
+    last_computer = total_data % 10
+
+    # 나머지가 0인 경우, 마지막 데이터는 10번 컴퓨터에 처리됩니다.
+    if last_computer == 0:
+        last_computer = 10
+
+    print(last_computer)
