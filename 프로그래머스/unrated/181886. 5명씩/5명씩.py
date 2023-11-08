1181b7a601f33c540f290769a38c00030561f679
mihyeon1234 <82564312+mihyeon1234@users.noreply.github.com>
2023-11-08 14:05:10 +0900

    [level unrated] Title: 5명씩, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181886. 5명씩/5명씩.py
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.py
@@ -1,8 +1,2 @@
 def solution(names):
-    answer = []
-    if len(names)<5:
-        return [names[0]]
-    else:
-        for i in range(1,len(names)+1,5):
-            answer.append(names[i-1])
-        return answer
+    return names[::5]
